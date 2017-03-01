aa0510ce2312811086aa1515d86c9ebd6fc3c636
Ian MacFarland <ian.macfarland@gmail.com>
2017-02-28 21:34:37 -0800

    : new comparison (diff) page

---
+++ js/compare/Total.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {HorizontalBar} from 'react-chartjs-2';
+import {entries} from 'd3-collection';
+import {asTick, asDiff, DiffStyled} from './utils';
+
+const chartOptions = {
+  legend: {
+    display: false,
+  },
+  scales: {
+    xAxes: [{
+      ticks: {
+        beginAtZero: true,
+        callback: value => {
+          // display as currency in billions
+          return `${asTick(value / 1000000000)}B`;
+        },
+      },
+    }]
+  }
+};
+
+export default class Total extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const totals = this.props.data;
+    let diff = totals[0].total - totals[1].total;
+    if (this.props.usePct) {
+      diff = diff / totals[1].total;
+    }
+    const data = {
+      labels: ['Total'],
+      datasets: totals.map((entry, i) => {
+        return {
+          data: [entry.total],
+          label: entry.key,
+          backgroundColor: this.props.colors[i],
+        };
+      }).reverse()
+    };
+
+    return <div>
+      <h3>Total Change:
+        <DiffStyled diff={diff} colors={this.props.diffColors} usePct={this.props.usePct}>
+        </DiffStyled>
+      </h3>
+      <HorizontalBar data={data} height={25} options={chartOptions}></HorizontalBar>
+    </div>
+  }
+}
